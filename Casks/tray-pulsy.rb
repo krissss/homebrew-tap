3f743500f3771385595fa9c6ae014393e004592d
kriss <462679766@qq.com>
2026-04-27 00:41:35 +0800

    Update tray-pulsy to 1.0.5

--- Casks/tray-pulsy.rb
+++ Casks/tray-pulsy.rb
@@ -1,8 +1,8 @@
 cask "tray-pulsy" do
-  version "1.0.4"
-  sha256 "195b10bb45f1314cb5c69a3fa4dcdabfd8e53e41ba6c7414ad038459f467b2b1"
+  version "1.0.5"
+  sha256 "b10acbc98cd533965016a430b81cc7bef855253ea30769e095969b957fca8aa3"
 
-  url "https://github.com/krissss/tray-pulsy/releases/download/v1.0.4/TrayPulsy-v1.0.4.dmg"
+  url "https://github.com/krissss/tray-pulsy/releases/download/v1.0.5/TrayPulsy-v1.0.5.dmg"
   name "TrayPulsy"
   desc "Lightweight macOS menu bar app with animated system monitor"
   homepage "https://github.com/krissss/tray-pulsy"
