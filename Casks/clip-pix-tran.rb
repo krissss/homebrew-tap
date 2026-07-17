9440c811141c50e605fa2576513eb46430a65528
kriss <462679766@qq.com>
2026-07-17 11:45:51 +0800

    Update clip-pix-tran to 0.6.2

--- Casks/clip-pix-tran.rb
+++ Casks/clip-pix-tran.rb
@@ -1,8 +1,8 @@
 cask "clip-pix-tran" do
-  version "0.6.1"
-  sha256 "a4442c9cf40a82ccc0674d2a27c75793341aaa2795cf6a4d157ad040a6a32443"
+  version "0.6.2"
+  sha256 "45cb383fe6be9a6493d0363347af137142856fdaadb77970c32a0322f280b7d0"
 
-  url "https://github.com/krissss/clip-pix-tran/releases/download/v0.6.1/ClipPixTran-v0.6.1.dmg"
+  url "https://github.com/krissss/clip-pix-tran/releases/download/v0.6.2/ClipPixTran-v0.6.2.dmg"
   name "ClipPixTran"
   desc "macOS clipboard, screenshot, and translation utility"
   homepage "https://github.com/krissss/clip-pix-tran"
